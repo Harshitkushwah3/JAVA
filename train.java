d9f420b7e943304d39f2b4bdb76a8c002eca2cee
HarshitKushwah03 <harshitkushwah51@gmail.com>
2023-10-01 08:24:30 +0530

    .

---
+++ train.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class train{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        a=5;
+        b=10;
+        c=a+b;
+        System.out.println(c);
+    }
+}
