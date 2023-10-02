46bb1b91d4a0899189a0b5374a6f363fdc74d5bc
HarshitKushwah03 <harshitkushwah51@gmail.com>
2023-10-02 08:14:13 +0530

    .

--- train.java
+++ train.java
@@ -1,11 +1,44 @@
-import java.util.*;
-class train{
-    public static void main(String[] args)
-    {
-        int a,b,c;
-        a=5;
-        b=10;
-        c=a+b;
-        System.out.println(c);
-    }
+/*public class train {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}*/
+
+/*PRINT NUMBAERS */
+
+import java.util.Scanner;
+
+class train {
+  public static void main(String[] args) {
+    System.out.println(3+3);
+    System.out.println(15*6);
+  }
+}
+
+class studnet {
+  public static void main(String[] args) {
+    int myNum = 5;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+String fname="Harshit";
+String lname="Kushwah";
+String fullname=fname+myNum;
+Scanner scn = new Scanner(System.in);
+int n=scn.nextInt();
+    System.out.println(n);
+    System.out.println(fullname);
+    System.out.println("hii"+myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+}
+
+class array{
+  public static void main(String[] args){
+
+  }
 }
