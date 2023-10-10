e88f88172e7ae55f15d0121734cad8214e884df0
HarshitKushwah03 <harshitkushwah51@gmail.com>
2023-10-10 15:30:15 +0530

    .

--- com/Harshit/AdavncePattern.java
+++ com/Harshit/AdavncePattern.java
@@ -55,9 +55,70 @@ public static void main(String[] args){
 
 //NUMBER PYRAMID
 
+/*
 public static void main(String[]args){
     int n=5;
-    
+    for (int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for (int j=1;j<=i;j++){
+            //System.out.print(j + " ");
+            //OR
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
+*/
+
+//PALINDROMIC PATTERN
+
+/*
+public static void main(String[] args){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for (int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for (int j=i;j>=1;j--){
+            System.out.print(j);
+        }
+        for(int j=2;j<=i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
+*/
+
+//DIAMOND PATTERN
+
+/*
+public static void main(String[] args){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for (int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for (int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    for(int i=n;i>=1;i--){
+        for (int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for (int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+
+    
+        System.out.println();
+    }
+}
+*/
+
 }
 
