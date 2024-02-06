225a86bad3ef26310582b233e607e003dd673aa4
HarshitKushwah03 <harshitkushwah51@gmail.com>
2024-02-06 15:44:18 +0530

    new

---
+++ ParcticeKG/prc_new.java
@@ -0,0 +1,13 @@
+package ParcticeKG;
+import java.util.*;
+
+
+class prc_new {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int n=sc.nextInt();
+    }
+ 
+    
+ }
