9d6aa53b37c79f46a827ce5499972858e6317d5e
HarshitKushwah03 <harshitkushwah51@gmail.com>
2024-02-08 13:04:04 +0530

    new

--- ParcticeKG/3_oops.java
+++ ParcticeKG/3_oops.java
@@ -2,6 +2,7 @@ package ParcticeKG;
 
 import java.util.Scanner;
 
+/* 
 // properties of the Car
 class Car {
     static int noOfCarsSold;
@@ -71,3 +72,65 @@ class Car {
                 '}';
     }
 }
+
+
+*/
+
+
+// PRACTICE OOPS
+
+/* 
+
+Create a Book class for a library system.
+    • Instance variables: title, author, isbn.
+    • Static variable: totalBooks, a counter for the total number of book 
+    instances.
+    • Instance methods: borrowBook(), returnBook().
+    • Static method: getTotalBooks(), to get the total number of books in the 
+    library
+
+*/
+
+ class Book{
+    static int totalBooks = 0;
+    String title;
+    String author;
+    String isbn;
+    boolean isborrowed;
+
+    { //Object init
+        totalBooks++;
+    }
+
+    Book(String title, String author, String isbn){ // parameterixed constructor
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+
+    }
+    Book(String isbn){
+        this(isbn,"unknown","unknown"); // parameterized  constructor
+    }
+
+    static int getTotalBooks(){
+        return totalBooks;
+    }
+    void borrowBook(){
+        if (isborrowed){
+            System.out.println(this.title + " is already borrowed.");
+        }
+        else{
+            System.out.println(this.title + " is borrowed.");
+            isborrowed = true;
+        }
+    }
+    void returnBook(){
+        if (!isborrowed){
+            System.out.println(this.title + " is not borrowed.");
+        }
+        else{
+            System.out.println(this.title + " is returned.");
+            isborrowed = false;
+        }
+    }
+ }
