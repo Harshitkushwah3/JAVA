023d1987b8932a97743f2e15c48fc28620b587dc
HarshitKushwah03 <harshitkushwah51@gmail.com>
2024-02-09 14:09:13 +0530

    .

---
+++ ParcticeKG/4_tern_string.java
@@ -0,0 +1,31 @@
+// TERNARY OPERATOR 
+
+package ParcticeKG;
+
+import java.util.Scanner;
+class ternary{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number: ");
+        int num1=sc.nextInt();
+        System.out.println("Enter 2nd number: ");
+        int num2 =sc.nextInt();
+        /*
+        if (num1>num2) {
+            System.out.println("The greater number is: "+num1);
+        } 
+        else {
+            System.out.println("The greater number is: "+num2);{
+            
+            }
+        }
+        */
+        int result = (num1>num2)?num1:num2; // ternary operator
+        System.out.println("The greater number is: " +result);
+         
+    }
+}
+
+class switcc{
+    psvm
+}
