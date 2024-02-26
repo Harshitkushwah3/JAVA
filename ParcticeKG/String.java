74d3678b3c1a5bf8f7dfa3ab3129bcbe1944e4f5
Harshitkushwah3 <harshitkushwah51@gmail.com>
2024-02-26 17:51:34 +0530

    .

--- ParcticeKG/String.java
+++ ParcticeKG/String.java
@@ -1,7 +1,6 @@
 package ParcticeKG;
 import java.util.*;
 
-
 class Sstring{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
@@ -34,9 +33,9 @@ class car{
         this.noOfDoor = noOfDoor;
         this.maxSpeed = maxSpeed;
         this.name = name;
-        this.modelNumebr = modelNumbers;
-        this.company = company;
+         this.company = company;
     }
+
     @Override
     public String toString() {
         return "my car name is " + name;
@@ -49,4 +48,55 @@ class car{
     }
 };
 
+class StringF{
+    public static void main(String[] args) {
+        String name="Harshit";
+        int marks =90;
+        System.out.printf("Hello %S you got %d percent",name,marks);
+    }
+}
 
+class sb{
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder();
+        s.append("TG ");
+        s.append("HK ");
+        System.out.println(s);
+    }
+}
+
+class finalk{
+
+    final double PI= 3.1412;
+
+    void testing(){
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+
+class jc{
+    String name;
+    String house;
+    String  color;
+    int age;
+    
+    public jc(String name, String house, String color, int age) {
+        this.name = name;
+        this.house = house;
+        this.color = color;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "jc [name=" + name + ", house=" + house + ", color=" + color + ", age=" + age + "]";
+    }
+    public static void main(String[] args) {
+        jc j1 = new jc("John", "Doe House", "Black", 25);
+        System.out.println(j1);
+    }
+    
+}
